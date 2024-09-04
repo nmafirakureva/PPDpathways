7a63dafbbcb4427e7ff87d35a6b6c221e222e1a4
Nyashadzaishe mafirakureva <n.mafirakureva@sheffield.ac.uk>
2024-09-04 12:27:43 +0100

    Updating parameters, DR & DRS files

--- R/runmodel.sh
+++ R/runmodel.sh
@@ -2,12 +2,9 @@
 # NOTE (after changing shell flag in ppd_simple.R to TRUE)
 # arg1: sensitivity analysis: none, base/lo/hi dscr, cdr (higher cdr for incidence), txd (completion of ATT/TPT included)
 
-R --slave --vanilla --args <ppd_simple.R screenAll & R --slave --vanilla --args <ppd_simple.R noltfu &
-# R --slave --vanilla --args <ppd_simple.R presumptiveTB & R --slave --vanilla --args <ppd_simple.R prisonGP &
-# R --slave --vanilla --args <ppd_simple.R clinicalSuspicion & R --slave --vanilla --args <ppd_simple.R attendNHS &
-# R --slave --vanilla --args <ppd_simple.R startATT & 
+R --slave --vanilla --args <ppd_simple.R AllattendNHS & R --slave --vanilla --args <ppd_simple.R noltfu &
 R --slave --vanilla --args <ppd_simple.R DOTsCost & R --slave --vanilla --args <ppd_simple.R XrayCost &
-R --slave --vanilla --args <ppd_simple.R FUVisitsCost & R --slave --vanilla --args <ppd_simple.R PrisonEscort & 
+R --slave --vanilla --args <ppd_simple.R FUVisitsCost & R --slave --vanilla --args <ppd_simple.R InpatientCost &
 R --slave --vanilla --args <ppd_simple.R none
 
 
